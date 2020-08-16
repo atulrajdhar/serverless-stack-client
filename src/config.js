75ed37bf79af6692dabaac29c5b8531a94beba4c
Atul Rajdhar <atulrajdhar@gmail.com>
2020-08-16 19:01:02 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "S3_UPLOADS_BUCKET_REGION",
+        BUCKET: "S3_UPLOADS_BUCKET_NAME"    
+    },
+    apiGateway: {
+        REGION: "API_GATEWAY_REGION",
+        URL: "API_GATEWAY_URL"
+    },
+    cognito: {
+        REGION: "COGNITO_REGION",
+        USER_POOL_ID: "COGNITO_USER_POOL_ID",
+        APP_CLIENT_ID: "COGNITO_APP_CLIENT_ID",
+        IDENTITY_POOL_ID: "IDENTITY_POOL_ID"
+    }
+};
